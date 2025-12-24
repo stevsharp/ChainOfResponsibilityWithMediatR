0b78f000d84092487d5aab503333b46a0752852d
SPYROS\Spyros <sponaris@gmail.com>
2025-12-24 11:58:18 +0200

    Add project files.

---
+++ ChainOfResponsibilityWithMediatR/Program.cs
@@ -0,0 +1,27 @@
+using MediatR;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services.AddMediatR(cfg =>
+{
+    cfg.RegisterServicesFromAssembly(typeof(Program).Assembly);
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+
+
+app.Run();
+
